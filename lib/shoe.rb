e5a0d80163a5e83331f759b75b9e971d0f75f042
Scott  Keplinger <skeplin17@gmail.com>
2020-05-13 21:02:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+
+class shoe
+
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "The shoe has been repaired!"
+  end
